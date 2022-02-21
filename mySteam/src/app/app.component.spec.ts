657d17718cb690f9c9deccd5c09f353d5cf37a51
Anton Chumak <Anton_Chumak@epam.com>
2022-02-21 11:56:03 +0200

    init new

--- mySteam/src/app/app.component.spec.ts
+++ mySteam/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mySteam'`, () => {
+  it(`should have as title 'MySteam'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mySteam');
+    expect(app.title).toEqual('MySteam');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mySteam app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MySteam app is running!');
   });
 });
--- mySteam/src/app/app.module.ts
+++ mySteam/src/app/app.module.ts
@@ -1,36 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule, Routes } from '@angular/router';
-import { MaterialModule } from './material/material.module';
-
-import { ProfileComponent } from './profile/profile.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LogInComponent } from './log-in/log-in.component';
-import { LibraryComponent } from './library/library.component';
-import { FriendsComponent } from './friends/friends.component';
-import { GamesComponent } from './games/games.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileComponent,
-    LogInComponent,
-    LibraryComponent,
-    FriendsComponent,
-    GamesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule,
-    AppRoutingModule,
-    MaterialModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-
 export class AppModule { }
