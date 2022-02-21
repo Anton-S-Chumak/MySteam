ce376cbfd05eef2121cb84d31086826ca0d3ebd6
Anton Chumak <Anton_Chumak@epam.com>
2022-02-21 12:36:57 +0200

    deploy5

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-steam'),
+      dir: require('path').join(__dirname, './coverage/git-anton_chumak-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MySteam'`, () => {
+  it(`should have as title 'git-anton_chumak-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MySteam');
+    expect(app.title).toEqual('git-anton_chumak-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MySteam app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('git-anton_chumak-angular app is running!');
   });
 });
